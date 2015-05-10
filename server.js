1c1e44047993f47ba64b5e725b53572e0efe8456
dkoleary88 <dan@dko.io>
2015-05-10 21:52:42 +1000

    add API functions

--- server.js
+++ server.js
@@ -32,3 +32,89 @@
 
 	app.listen(8080);
 	console.log('App is listening on port 8080');
+
+// routes ========================================
+
+	// api ---------------------------------------
+	// get all todos
+
+	app.get('/api/todos', function(req, res){
+
+		// use mongoose to find all Todos
+		Todo.find(function(err, todos){
+
+			// if there is an error, send the error
+			if (err){
+				res.send(err);
+			}
+
+			res.json(todos);
+
+		});
+	});
+
+	// create a todo and send back all todos after creation
+
+	app.post('/api/todos', function(req, res){
+
+		// use mongoose to insert new todo (from AJAX req from Angular) into DB
+		Todo.create({
+			text : req.body.text,
+			done : false
+		},function(err, todo){
+			if(err){
+				res.send(err);
+			}
+
+			// show all todos
+			Todo.find(function(err, todos){
+				if(err){
+					res.send(err);
+				}
+
+				res.json(todos);
+			});
+
+		});
+	});
+
+	// delete a todo
+
+	app.delete('/api/todos/:todo_id',function(req, res){
+
+		Todo.remove({
+			_id : req.params.todo_id
+		},function(err,todo){
+			if (err){ res.send(err); }
+
+			Todo.find(function(err, todos){
+				if (err){ res.send(err); }
+
+				res.json(todos);
+			});
+		});
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
