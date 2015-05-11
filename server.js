50464c859ec9f9ef03b7a302d6c2c0f85c516d26
dkoleary88 <dan@dko.io>
2015-05-11 20:38:18 +1000

    compartmentalise the server API

--- server.js
+++ server.js
@@ -11,8 +11,9 @@
 
 
 	// configuration -----------------------------
-
-	mongoose.connect('mongodb://localhost');
+		// db config -----
+		var database = require('./config/database');
+		mongoose.connect(database.url);
 
 	app.use(express.static(__dirname + '/public'));
 	app.use(morgan('dev'));
@@ -21,89 +22,12 @@
 	app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 	app.use(methodOverride());
 
-	// Todo mongoose model -----------------------
-
-	var Todo = mongoose.model('Todo', {
-		text : String
-	});
-
+	// load the routes
+	require('./app/routes')(app);
 
 	// listen - start app with node server.js ----
-
 	app.listen(8080);
 	console.log('App is listening on port 8080');
-
-// routes ========================================
-
-	// front-end route ---------------------------
-
-	app.get('*', function(req, res){
-
-		res.sendfile('./public/index.html');
-
-	});
-
-	// api ---------------------------------------
-	// get all todos
-
-	app.get('/api/todos', function(req, res){
-
-		// use mongoose to find all Todos
-		Todo.find(function(err, todos){
-
-			// if there is an error, send the error
-			if (err){
-				res.send(err);
-			}
-
-			res.json(todos);
-
-		});
-	});
-
-	// create a todo and send back all todos after creation
-
-	app.post('/api/todos', function(req, res){
-
-		// use mongoose to insert new todo (from AJAX req from Angular) into DB
-		Todo.create({
-			text : req.body.text,
-			done : false
-		},function(err, todo){
-			if(err){
-				res.send(err);
-			}
-
-			// show all todos
-			Todo.find(function(err, todos){
-				if(err){
-					res.send(err);
-				}
-
-				res.json(todos);
-			});
-
-		});
-	});
-
-	// delete a todo
-
-	app.delete('/api/todos/:todo_id',function(req, res){
-
-		Todo.remove({
-			_id : req.params.todo_id
-		},function(err,todo){
-			if (err){ res.send(err); }
-
-			Todo.find(function(err, todos){
-				if (err){ res.send(err); }
-
-				res.json(todos);
-			});
-		});
-
-	});
-
 
 
 
